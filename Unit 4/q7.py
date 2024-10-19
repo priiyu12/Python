037115c1429a862698914d47b0fef93411c83208
priiyu12 <136467028+priiyu12@users.noreply.github.com>
2024-10-19 08:20:09 +0530

    Update q7.py

--- Unit 4/q7.py
+++ Unit 4/q7.py
@@ -1,13 +1,14 @@
+class StockError(Exception):
+	pass
+
 def check_stock(rquantity,available):
 	if rquantity>available:
-		raise ValueError("Cannot be requested")
+		raise StockError("Cannot be requested")
 
 try:
 	available=int(input("Enter stock: "))
 	rquantity=int(input("Enter requested stock: "))
-
 	check_stock(rquantity,available)
 	print("Succesfully requested: ",rquantity)
-
-except ValueError as e:
+except StockError as e:
 	print(e)
